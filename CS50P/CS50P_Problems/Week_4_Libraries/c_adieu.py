b75d7ce044b19b22a29bbac3b4d24ed007048f71
rinkal159 <rinkalsingapuri@gmail.com>
2026-05-26 19:03:08 +0530

    Bids 'adieu' to user entered names formated by inflect.

---
+++ CS50P/CS50P_Problems/Week_4_Libraries/c_adieu.py
@@ -0,0 +1,16 @@
+import inflect
+p = inflect.engine()
+
+print(p.number_to_words(100), andword="")
+
+# nameList = []
+
+# while True:
+#     try:
+#         nameList.append(input("Name: "))
+#     except EOFError:
+#         print()
+#         break
+
+# print(f"Adieu, adieu, to {inflectEngine.join(nameList)}")
+
